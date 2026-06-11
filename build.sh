d555a09decd5a5e3dbf0393df8e4338aca60a7d5
aco4 <anthonythegu@gmail.com>
2026-06-11 00:17:00 -0400

    Add hardpoints

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 python3 main.py --scale 2 --inputs \
     wheeled01 HalfTrack tracked01 hover01 V-Tol \
     Body1REC Body5REC Body11ABT Body4ABT Body8MBT Body12SUP Body2SUP Body6SUPP Body9REC Body3MBT Body7ABT Body10MBT Body13SUP Body14SUP \
-    Cannon1Mk1 Cannon2A-TMk1 Cannon5VulcanMk1 Cannon375mmMk1 Cannon6TwinAslt \
+    Cannon1Mk1 Cannon2A-TMk1 Cannon4AUTOMk1 Cannon5VulcanMk1 Cannon375mmMk1 Cannon6TwinAslt \
     Rocket-Pod Rocket-MRL Rocket-LtA-T Rocket-MRL-Hvy Rocket-HvyA-T \
     Flame2 Flame1Mk1 \
     MG2Mk1 MG3Mk1 MG4ROTARYMk1 MG5TWINROTARY \
-    PillBox1 PillBox4 PillBox5 GuardTower6 Emplacement-MortarPit01 X-Super-Cannon X-Super-Rocket Mortar1Mk1 \
+    PillBox1 PillBox4 PillBox5 GuardTower6 Emplacement-MortarPit01 X-Super-Cannon X-Super-Rocket Mortar1Mk1 WallTower01 WallTower02 WallTower03 WallTower06 \
     --vanillapropulsion --research
